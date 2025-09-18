e3b8b67671edee930a203a04318eb43707a0f4fb
Chetan <chetankiran9666@gmail.com>
2025-09-18 18:38:17 +0530

    second push  frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import GFG_scraper from "./GFG_scraper";
+import "./GFG_scraper.css";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app-root">
       <main className="app-content">
-        <h1 style={{marginLeft:540}}>GFG Leaderboard Scraper</h1>
+        <h1 className="heading" style={{ color: "white" }}>
+          GFG Leaderboard Scraper
+        </h1>
         <GFG_scraper />
       </main>
       <Footer />
--- frontend/src/GFG_scraper.jsx
+++ frontend/src/GFG_scraper.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./GFG_scraper.css";
 
 import {
   Chart as ChartJS,
